e73ff9e62f2a56e849b3c1447072e55a887d3a8e
SkyPro <vadim.phobia@gmail.com>
2022-04-29 23:13:00 +0300

    Выполнил ДЗ #4 + сложные задачи

--- src/Main.java
+++ src/Main.java
@@ -40,5 +40,32 @@ public class Main {
                 System.out.println(cometVisitUs);
             }
         }
+
+        //Lesson 4 Task 4
+        String result;
+        for (int x = 1; x < 31; x++) {
+            result = (x + ":");
+            if (x % 3 == 0)
+                result = result + " ping";
+            if (x % 5 == 0)
+                result = result + " pong";
+            System.out.println(result);
+        }
+
+        //Lesson 4 Task 5
+
+        int i = 0;
+        int j = 1;
+        int temp;
+        int k;
+        System.out.print(i + " ");
+        for (k = 0; k < 9; k++) {
+            System.out.print(j + " ");
+            temp = i ;
+            i = j;
+            j = temp + j;
+        }
+
+
     }
 }
